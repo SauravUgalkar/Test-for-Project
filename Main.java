33dbb5e73204d020e128fb9642d9514c1d5971cc
demo1 <demo1@gmail.com>
2026-04-02 18:37:17 +0000

    Checkpoint before switching to master

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String ar[]){
+        Sum obj = new Sum();
+        System.out.println("Addition of a and b is : " + obj.c);
+        System.out.println("Hello world");
+    }
+}
---
+++ Sum.java
@@ -0,0 +1,5 @@
+public class Sum{
+    public int a =20;
+    public int b =30;
+    public int c=a+b;
+}
